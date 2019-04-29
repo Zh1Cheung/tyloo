0e01e6bbc11331f3ad9cd98a936285d4acef7594
张子恒 <35570187+Zh1Cheung@users.noreply.github.com>
2019-04-29 15:08:11 +0800

    更新core模块
    新增方法角色类

---
+++ tyloo-core/src/main/java/io/tyloo/common/MethodRole.java
@@ -0,0 +1,26 @@
+package io.tyloo.common;
+
+/*
+ * 方法角色
+ *
+ * @Author:Zh1Cheung zh1cheunglq@gmail.com
+ * @Date: 15:27 2019/4/15
+ *
+ */
+public enum MethodRole {
+
+    /**
+     * 根
+     */
+    ROOT,
+
+    /**
+     * 提供者
+     */
+    PROVIDER,
+
+    /**
+     * 常规
+     */
+    NORMAL;
+}
