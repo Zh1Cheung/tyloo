f49dfef8270eb71acac429e345f47be9a5f1d648
张子恒 <35570187+Zh1Cheung@users.noreply.github.com>
2019-05-12 15:29:59 +0800

    注解方法上下文
    新增了几个异常类

---
+++ tyloo-core/src/main/java/io/tyloo/NoExistedTransactionException.java
@@ -0,0 +1,11 @@
+package io.tyloo;
+
+/*
+ *
+ * @Author:Zh1Cheung zh1cheunglq@gmail.com
+ * @Date: 19:55 2019/6/1
+ *
+ */
+public class NoExistedTransactionException extends Exception {
+    private static final long serialVersionUID = 1031919168789207713L;
+}
---
+++ tyloo-core/src/main/java/io/tyloo/OptimisticLockException.java
@@ -0,0 +1,11 @@
+package io.tyloo;
+
+/*
+ *
+ * @Author:Zh1Cheung zh1cheunglq@gmail.com
+ * @Date: 19:55 2019/6/1
+ *
+ */
+public class OptimisticLockException extends RuntimeException {
+    private static final long serialVersionUID = -4250747002159020605L;
+}
