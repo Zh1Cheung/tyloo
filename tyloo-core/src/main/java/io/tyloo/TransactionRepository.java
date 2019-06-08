840835b82824689b3f80d5a6e60d0b61c641b1ca
张子恒 <35570187+Zh1Cheung@users.noreply.github.com>
2019-06-08 15:35:51 +0800

    更新core模块
    新增事务repository接口 序列化类

---
+++ tyloo-core/src/main/java/io/tyloo/TransactionRepository.java
@@ -0,0 +1,31 @@
+package io.tyloo;
+
+import io.tyloo.api.TransactionXid;
+
+import java.util.Date;
+import java.util.List;
+
+/*
+ *
+ * 事务库(连接tcc数据库的dao)
+ * 真实对象在本地项目dubbo-order中的config.spring.local中appcontext-service-dao.xml有配置，
+ * 其真实类型是SpringJdbcTransactionRepository，继承JdbcTransactionRepository，因为order、capital、redpacket是3个独立的服务，
+ * 所以每一个模块中都有appcontext-service-dao.xml来配置连接池
+ *
+ * @Author:Zh1Cheung zh1cheunglq@gmail.com
+ * @Date: 19:00 2019/6/12
+ *
+ */
+
+public interface TransactionRepository {
+
+    int create(Transaction transaction);
+
+    int update(Transaction transaction);
+
+    int delete(Transaction transaction);
+
+    Transaction findByXid(TransactionXid xid);
+
+    List<Transaction> findAllUnmodifiedSince(Date date);
+}
